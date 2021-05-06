463e579ad4ef31f14140aa2dc7709609542139d2
apresthus <apresthus@gmail.com>
2021-05-06 12:27:56 +0200

    board setup & configuration

--- src/Board.svelte
+++ src/Board.svelte
@@ -1,6 +1,31 @@
 <script>
+  import { onMount } from "svelte";
+
+  /////////////////////////////////
+  /*
+ 64SQ "STANDARD" BOARD CONVERTED TO 120SQ BOARD WITH OFFBOARD SQUARES DEFINED AS 100
+
+100 100 100 100 100 100 100 100 100 100   
+100 100 100 100 100 100 100 100 100 100 
+100 21  22  23  24  25  26  27  28  100 
+100 31  32  33  34  35  36  37  38  100 
+100 41  42  43  44  45  46  47  48  100 
+100 51  52  53  54  55  56  57  58  100 
+100 61  62  63  64  65  66  67  68  100 
+100 71  72  73  74  75  76  77  78  100
+100 81  82  83  84  85  86  87  88  100 
+100 91  92  93  94  95  96  97  98  100
+100 100 100 100 100 100 100 100 100 100 
+100 100 100 100 100 100 100 100 100 100 
+
+NOTE: BOARD IS "UPSIDE DOWN" WITH A1 = 21 AND B8 = 92 AND SO ON.
+
+*/
+  ////////////////////////////////////
+
   const COMPLETE_BOARD_SIZE = 120;
   const STANDARD_BOARD_SIZE = 64;
+
   const FILES = {
     A: 0,
     B: 1,
@@ -24,7 +49,7 @@
     NO_RANK: 8,
   };
 
-  const KEY_SQRS = {
+  const SQUARES = {
     A1: 21,
     B1: 22,
     C1: 23,
@@ -48,24 +73,45 @@
   const COLORS = { WHITE: 0, BLACK: 1, BOTH: 2 };
 
   const PIECE_TYPES = {
-    wP: 0,
-    wNg: 1,
-    wB: 2,
-    wR: 3,
-    wQ: 4,
-    wK: 5,
-    bP: 6,
-    bNg: 7,
-    bB: 8,
-    bR: 9,
-    bQ: 10,
-    bK: 11,
-    NO_PCE: 12,
+    wP: 1,
+    wNg: 2,
+    wB: 3,
+    wR: 4,
+    wQ: 5,
+    wK: 6,
+    bP: 7,
+    bNg: 8,
+    bB: 9,
+    bR: 10,
+    bQ: 11,
+    bK: 12,
+    NO_PCE: 0,
   };
-  //initialize the gameboard with all squares being off board
-  export let Board = Array(COMPLETE_BOARD_SIZE).fill(KEY_SQRS.OFFBOARD);
 
-  function rankFileToSqr(file, rank) {
+  //initialize the gameboard with all squares being off board
+  let FILES_BOARD = Array(COMPLETE_BOARD_SIZE).fill(SQUARES.OFFBOARD);
+  let RANKS_BOARD = Array(COMPLETE_BOARD_SIZE).fill(SQUARES.OFFBOARD);
+  let currentFile = FILES.A;
+  let currentRank = RANKS.RANK_1;
+
+  onMount(async () => {
+    console.log("mounting");
+    initBoard();
+  });
+  function initBoard() {
+    console.log("initialising board");
+    let sq = SQUARES.A1;
+
+    for (let rank = RANKS.RANK_1; rank <= RANKS.RANK_8; rank++) {
+      for (let file = FILES.A; file <= FILES.H; file++) {
+        sq = fileRankToSqr(file, rank);
+        FILES_BOARD[sq] = file;
+        RANKS_BOARD[sq] = rank;
+      }
+    }
+  }
+  function fileRankToSqr(file, rank) {
+    console.log(21 + file + rank * 10);
     return 21 + file + rank * 10;
   }
 </script>
